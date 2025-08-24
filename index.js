0e7be8ad077d41cfd20edfc7511f4a488d20f7f5
Atchariya <atchariya.nna@gmail.com>
2025-08-24 05:12:15 +0000

    chore: test code for deploy to vercel

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log("server is running on port " + PORT)
+});
+
+app.get("/", (req, res) => {
+    res.send("test deploy")
+});
+
+app.get("/test", (req, res) => {
+    res.send("test in test")
+})
+
+export default app;
